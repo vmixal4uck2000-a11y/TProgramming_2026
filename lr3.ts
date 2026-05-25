3fd6b9c65fd7c3468854c35c5460c8032ab57cf7
vmixal4uck2000-a11y <v.mixal4uck2000@mail.ru>
2026-05-25 22:48:02 +0300

    Update lr3.ts

--- lr3.ts
+++ lr3.ts
@@ -3,7 +3,6 @@ function calculate(a: number, b: number, x: number): number {
     const niz: number = Math.pow(Math.log10(x), 2);
     return Math.sqrt(verh / niz);
 }
-
 function taskA(a: number, b: number, xStart: number, xEnd: number, deltax: number): void {
     console.log("\nзадание А")
     console.log(calculate(a, b, xStart));
@@ -13,7 +12,6 @@ function taskA(a: number, b: number, xStart: number, xEnd: number, deltax: numbe
     console.log(calculate(a, b, xStart + deltax * 4));
     console.log(calculate(a, b, xEnd));
 }
-
 function taskB(a: number, b: number, x1: number, x2: number, x3: number, x4: number, x5: number): void {
     console.log("\nзадание B")
     console.log(calculate(a, b, x1));
@@ -22,8 +20,5 @@ function taskB(a: number, b: number, x1: number, x2: number, x3: number, x4: num
     console.log(calculate(a, b, x4));
     console.log(calculate(a, b, x5));
 }
-
-
-
 taskA(7.2, 4.2, 1.81, 5.31, 0.7);
 taskB(7.2, 4.2, 2.4, 2.8, 3.9, 4.7, 3.16);
